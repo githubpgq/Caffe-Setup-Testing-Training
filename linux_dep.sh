1e9c0e2d7faef47c0924e7ce4233c8dc9d46c597
omair474 <omair.hassaan@gmail.com>
2016-04-19 17:10:19 +0500

    Delete linux_dep.sh

--- linux_dep.sh
+++
@@ -1,2 +0,0 @@
-sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev protobuf-compiler gfortran libjpeg62 libfreeimage-dev libatlas-base-dev git python-dev python-pip libgoogle-glog-dev libbz2-dev libxml2-dev libxslt-dev libffi-dev libssl-dev libgflags-dev liblmdb-dev python-yaml && sudo easy_install pillow
-
